e2e232c9761ed8e53b75aca0a5bb90fbd394f6eb
Carlos <carloswelliton7@gmail.com>
2025-07-24 15:12:28 -0400

    atualizando o .env e configurando o banco para puxar as credentials corretas

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,8 +1,19 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../chatbot-gold.json');
+require('dotenv').config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: 'service_account',
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
 });
 
 const db = admin.firestore();
